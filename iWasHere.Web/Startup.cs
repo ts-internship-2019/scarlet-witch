6510cc0549fda4a3eeecff9d1bf405975cad6382
Alexandru Stefan <astefan@totalsoft.ro>
2019-09-12 14:13:26 +0300

    Revert "testalec"
    This reverts commit 977b0e39cc9ebb1fa77db849198f1043951f0f85.

--- iWasHere.Web/Startup.cs
+++ iWasHere.Web/Startup.cs
@@ -26,7 +26,7 @@ namespace iWasHere.Web
         }
 
         public IConfiguration Configuration { get; }
-        //comm de la alec
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
