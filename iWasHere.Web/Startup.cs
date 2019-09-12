c46e94d1ea2e074aa220240a5d312a40abd50952
Alice Draghici <alicedraghici597@gmail.com>
2019-09-12 12:21:22 +0300

    AliceTest

--- iWasHere.Web/Startup.cs
+++ iWasHere.Web/Startup.cs
@@ -25,6 +25,7 @@ namespace iWasHere.Web
             Configuration = configuration;
         }
 
+        //Alice Test 1st Day
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
