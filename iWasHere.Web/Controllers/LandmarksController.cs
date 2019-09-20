e366461d5bd6618e25ce7f3f254866a7b7060ca1
achirca <achirca@TS1256>
2019-09-20 18:07:07 +0300

    ti-43 image upload locallly

--- iWasHere.Web/Controllers/LandmarksController.cs
+++ iWasHere.Web/Controllers/LandmarksController.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using iWasHere.Domain.DTOs;
 using iWasHere.Domain.Service;
 using Kendo.Mvc.Extensions;
 using Kendo.Mvc.UI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace iWasHere.Web.Controllers
@@ -13,10 +17,12 @@ namespace iWasHere.Web.Controllers
     public class LandmarksController : Controller
     {
         private readonly DictionaryService _dictionaryService;
+        private readonly IHostingEnvironment _he;
 
-        public LandmarksController(DictionaryService dictionaryService)
+        public LandmarksController(DictionaryService dictionaryService, IHostingEnvironment he)
         {
             _dictionaryService = dictionaryService;
+            _he = he;
         }
         public IActionResult LandmarkList()
         {
@@ -52,6 +58,42 @@ namespace iWasHere.Web.Controllers
             };
 
             return Json(result);
+        }
+
+        public void SaveImage(IFormFile pic)
+        {
+            if( pic!= null)
+            {
+            
+                var fileName = Path.Combine(_he.WebRootPath, Guid.NewGuid().ToString() + Path.GetExtension(pic.FileName));
+                pic.CopyTo(new FileStream(fileName, FileMode.Create));
+            }
+        }
+        private IEnumerable<string> GetFileInfo(IEnumerable<IFormFile> files)
+        {
+            List<string> fileInfo = new List<string>();
+
+            foreach (var file in files)
+            {
+                var fileContent = ContentDispositionHeaderValue.Parse(file.ContentDisposition);
+                var fileName = Path.GetFileName(fileContent.FileName.ToString().Trim('"'));
+
+                fileInfo.Add(string.Format("{0} ({1} bytes)", fileName, file.Length));
+            }
+
+            return fileInfo;
+        }
+        public ActionResult Submit(IFormFile files)
+        {
+            IEnumerable<string> fileInfo = new List<string>();
+
+            if (files != null)
+            {
+                var fileName = Path.Combine(_he.WebRootPath, Path.GetFileName(files.FileName));
+                files.CopyTo(new FileStream(fileName, FileMode.Create));
+            }
+
+            return View();
         }
 
 
